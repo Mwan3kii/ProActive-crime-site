46dcedce54d8841b2ab9845a5bc8a6be87b24c96
Mwan3kii <agathawanja303@gmail.com>
2024-07-10 17:10:19 +0300

    Added comments on app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+"""Initializes the main entry point of the Flask application"""
 from website import create_app
 
+# Check if this script is being run directly
 if __name__ == "__main__":
+    # Create an instance of the Flask application using create_app function
     app = create_app()
     app.run(debug=True)
